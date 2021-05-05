cfce1df09d4b7f8ef3c54800b64e1e118295ff84
Jasson9 <52589181+Jasson9@users.noreply.github.com>
2021-05-05 16:49:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const GIMG = new GoogleImages('9158f798cdfa53799', 'AIzaSyAG6gTt_12cJjlqBUH6-bq8
 const defaultAccessToken = '***********************';
 const defaultSecret = '***********************';
 
+
+//settings
+const prefix ="."
+
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || defaultAccessToken,
@@ -31,9 +35,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
 });
-//var msg =".picture test"
-//var args = [].slice.call(msg);
-//console.log(args)
+
 // event handler
 function handleEvent(data) {
 var event=JSON.parse(JSON.stringify(data))
@@ -42,29 +44,33 @@ var event=JSON.parse(JSON.stringify(data))
     // ignore non-text-message event
     return Promise.resolve(null);
   }
-
+  var args = event.split(" ")
+  var cmd = args.shift().replace(prefix,"")
   // create a echoing text message
-  if(event.message.text=="echo"){
-  var echo = { type: 'text', text: "echo" };
-  
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  switch(cmd){
+    case "help":
+     client.replyMessage(event.replyToken,{type:'text',text: `available commands: \n ${prefix}help:show this message \n ${prefix}picture:search picture using google search engine \n ${prefix}echo: reply back the message \n \n creator:JZ9`})
+      break;
+      case "picture":
+        if(!args[0].length){client.replyMessage(event.replyToken,{type:'text',text:"no keyword"});return}
+        try{
+          GIMG.search(args[0].then(images => {
+          if(!images){client.replyMessage(event.replyToken,{type:'text',text:"request has reached the limit"});return}
+            client.replyMessage(event.replyToken,{type:'image', originalContentUrl:images[0].url,previewImageUrl:images[0].thumbnail.url})
+        }))    
+        }
+        catch(err){if(err){
+          console.log(err)}}
+        break;
+      case "echo":
+          if(!args[0].length){
+            client.replyMessage(event.replyToken,{type:'text',text:"no message specified"});return
+          }
+          client.replyMessage(event.replyToken,{type:'text',text:args[0]});break;
+    default:
+     client.replyMessage(event.replyToken,{type:'text',text:`no command use ${prefix}help for more info`})
   }
-  if(event.message.text.includes(".pic")){ 
-    var keyword = event.message.text.replace(".pic ","")
-GIMG.search(keyword)
-    .then(images => {
-      if(!images){client.replyMessage(event.replyToken,{type:'text',text:"request has reached the limit"});return}
-        client.replyMessage(event.replyToken,{type:'image', originalContentUrl:images[0].url,previewImageUrl:images[0].thumbnail.url})
-    });
 }
-if(event.message.text.includes(".picture")){ 
-    var keyword = event.message.text.replace(".picture ","")
-GIMG.search(keyword)
-    .then(images => {
-        client.replyMessage(event.replyToken,{type:'image', originalContentUrl:images[0].url,previewImageUrl:images[0].thumbnail.url})
-    });
-}}
 // listen on port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
