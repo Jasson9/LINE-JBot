fe8544989770a374e2f60d8531644fd6530f232c
Jasson9 <52589181+Jasson9@users.noreply.github.com>
2021-05-09 15:33:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -62,11 +62,11 @@ function hangmangame(Token,GID,word){
   console.log(Token,GID,word, show, order)
    client.replyMessage(Token,{type:'text',text:`guess this word \n ${show}` })
    setTimeout(() => {
-    show =show[order[1]]=word[order[1]] 
+    show[order[1]]=word[order[1]] 
     client.replyMessage(Token,{type:'text',text:`guess this word \n ${show}`})
   }, 30000)
    setTimeout(()=>{
-    show =show[order[2]]=word[order[2]] 
+    show[order[2]]=word[order[2]] 
      client.replyMessage(Token,{type:'text',text:`guess this word \n ${show}`})
   },45000)
   setTimeout(()=>{
