54ddbaa0081ddb93d0e1461e7adafc3947060b3e
Jasson9 <52589181+Jasson9@users.noreply.github.com>
2021-05-10 14:24:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -67,43 +67,54 @@ for (let i = 0;i<data.length; i++) {
   var show = "-".repeat(word.length).split('')
   show[order[0]]=word[order[0]] 
   console.log(Token,GID,word, show, order)
-   client.replyMessage(Token,{type:"text",text:`guess this word and save this person \n ${show.join('')} \n \n you have 90 seconds`}).then(()=>
+   client.replyMessage(Token,{type:"text",text:`guess this word and save this person \n ${show.join('')} \n \n you have 90 seconds`})
+
    setTimeout(() => { //15 seconds
      if(hangman.includes(GID)){
       if(count(show)>2){
-    show[order[1]]=word[order[1]] }
+    show[order[1]]=word[order[1]] 
     client.pushMessage(GID,{type:"text",text:`75 seconds left! \n ${show.join('')} \n \n \n / `})
-  }else{return}}, 15000)).then(()=>
+
    setTimeout(()=>{
       // 30 seconds
     if(count(show)>2){
-    show[order[2]]=word[order[2]] }
+    show[order[2]]=word[order[2]] 
      client.pushMessage(GID,{type:"text",text:`60 seconds left! \n ${show.join('')} \n\n  \n / \\`})
-   },15000)).then(()=>
-   setTimeout(()=>{
-    if(hangman.includes(GID)){ // 45 seconds
-   if(count(show)>2){
-   show[order[3]]=word[order[3]] }
-    client.pushMessage(GID,{type:"text",text:`45 seconds left! \n ${show.join('')}\n\n  |\n / \\ `})
-  }else{return}},15000)).then(()=>
-  setTimeout(()=>{
-    if(hangman.includes(GID)){ // 60 seconds
-   if(count(show)>2){
-   show[order[4]]=word[order[4]] }
-    client.pushMessage(GID,{type:"text",text:`30 seconds left! \n ${show.join('')}\n\n /|\n / \\ `})
-  }else{return}},15000)).then(()=>
-  setTimeout(()=>{
-    if(hangman.includes(GID)){ // 75 seconds
-   if(count(show)>2){
-   show[order[5]]=word[order[5]] }
-    client.pushMessage(GID,{type:"text",text:`15 seconds left! \n ${show.join('')}\n\n /|\\\n / \\ `})
-  }else{return}},15000)).then(()=>
-  setTimeout(()=>{ //dead
-    if(hangman.includes(GID)){
-     client.pushMessage(GID,{type:'text',text:`owh no he is dead! the answer is ${word} \n\n  |\n  X\n /|\\\n / \\ `})
-    hangman.splice(hangman.indexOf(GID),1);
-    return
-  }return},15000));
+
+     setTimeout(()=>{
+      if(hangman.includes(GID)){ // 45 seconds
+     if(count(show)>2){
+     show[order[3]]=word[order[3]] }
+      client.pushMessage(GID,{type:"text",text:`45 seconds left! \n ${show.join('')}\n\n  |\n / \\ `})
+  
+    setTimeout(()=>{
+      if(hangman.includes(GID)){ // 60 seconds
+     if(count(show)>2){
+     show[order[4]]=word[order[4]] }
+      client.pushMessage(GID,{type:"text",text:`30 seconds left! \n ${show.join('')}\n\n /|\n / \\ `})
+    
+    setTimeout(()=>{
+      if(hangman.includes(GID)){ // 75 seconds
+     if(count(show)>2){
+     show[order[5]]=word[order[5]] }
+      client.pushMessage(GID,{type:"text",text:`15 seconds left! \n ${show.join('')}\n\n /|\\\n / \\ `})
+    setTimeout(()=>{ //dead
+      if(hangman.includes(GID)){
+       client.pushMessage(GID,{type:'text',text:`owh no he is dead! the answer is ${word} \n\n  |\n  X\n /|\\\n / \\ `})
+      hangman.splice(hangman.indexOf(GID),1);
+      return
+    }else{return}
+  },15000)
+}else{return}
+  },15000)
+}else{return}
+  },15000)
+}else{return}
+  },15000)  
+}else{return}
+  },15000)
+}else{return}
+  }}, 15000);
   setInterval(() => {
     if(!hangman.includes(GID)){return;}
   }, 2000);
