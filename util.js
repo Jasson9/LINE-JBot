f38d1e53043e8d989d27fca3573d6530a1cdb4f0
Jasson9 <52589181+Jasson9@users.noreply.github.com>
2021-10-02 14:02:40 +0700

    add config file for each chat

--- util.js
+++ util.js
@@ -1,13 +1,17 @@
-
+const fs =require('fs')
 let config;
-
-let IDS = {}
+let IDS = require('./IDSconfig.json');
+function write(){
+  var data =  JSON.stringify(IDS)
+  fs.writeFileSync("./IDSconfig.json",data);
+  return
+}
 try {
     config = require("./config.json");
   } catch (error) {
     config = null;
   }
-
+setInterval(write,10000)
 exports.IDS = IDS;
 exports.PREFIX = process.env.PREFIX||config.PREFIX;
 exports.SEARCH_ENGINE = process.env.SEARCH_ENGINE||config.search_engine;
